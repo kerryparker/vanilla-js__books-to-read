7a0f3775953c8c3309efe9221e3c2a79d7fb5718
Kerry Parker <kerry.not.kerry@gmail.com>
2021-04-19 00:56:02 +0300

    book counter added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let addButton = document.getElementById("addButton");
 let readList = document.getElementById("readListHolder");
+let doneCounter = [];
+let bookCounter = [];
+let btnDoneCounter = document.querySelector(".done-counter");
+let btnBookCounter = document.querySelector(".books-counter");
 
 addButton.addEventListener("click", addToRead);
 readList.addEventListener("click", removeOrMarkRead);
@@ -12,20 +16,24 @@ function addToRead(event) {
     <p class="read-list__author">Author</p>
     <button class="mark-btn">Mark as read</button><button class="remove-btn">Remove from list</button>`;
   readList.appendChild(div);
+  bookCounter.push(div);
+  console.log(bookCounter);
+  btnBookCounter.innerText = bookCounter.length;
 }
 
 function removeOrMarkRead(e) {
   const item = e.target;
+  const listItem = item.parentElement;
   if (item.className === "remove-btn") {
-    const listItem = item.parentElement;
     listItem.remove();
-  } else
-   if (item.className === "mark-btn") {
-      const listItem = item.parentElement;
-      listItem.classList.toggle("marked-item");
-      let button = listItem.querySelectorAll('button');
-      button[1].remove();
-      item.remove();
-      
+    bookCounter.pop();
+    btnBookCounter.innerText = bookCounter.length;
+  } else if (item.className === "mark-btn") {
+    listItem.classList.toggle("marked-item");
+    doneCounter.push(listItem);
+    btnDoneCounter.innerText = doneCounter.length;
+    let button = listItem.querySelector(".remove-btn");
+    button.remove();
+    item.remove();
   }
 }
